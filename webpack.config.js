b75b0dc00675623c2b8150191a5de0d75e268d95
Jon Basco <joncaesar@gmail.com>
2022-12-06 21:03:34 +0800

    Copied kuys pat's update for npm dev.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path')
-
 module.exports = {
     entry: "./app/assets/scripts/App.js",
     output: {
         filename: "bundled.js",
         path: path.resolve(__dirname, "app")
     },
-
+    devServer: {
+        before: function (app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
+    mode: "development",
     module: {
         rules: [
             {
